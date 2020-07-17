06a38b520f81aaefb501dee1b3de7d21f88bb9b0
Human Gamer <humangamer.dev@hotmail.com>
2020-07-17 17:30:19 -0500

    Commandline Arguments

--- GameIdeaGenerator/Program.cs
+++ GameIdeaGenerator/Program.cs
@@ -6,10 +6,30 @@ namespace GameIdeaGenerator
     {
         public static void Main(string[] args)
         {
+            string templateFile = "basic";
+            if (args.Length > 0)
+            {
+                templateFile = args[0];
+            }
+
+            int count = 1;
+            if (args.Length > 1)
+            {
+                if (!int.TryParse(args[1], out count))
+                {
+                    Console.WriteLine("Invalid Argument: " + args[1]);
+                    return;
+                } else
+                {
+                    if (count < 1)
+                        Console.WriteLine("Invalid Argument: " + args[1] + ", count must be positive!");
+                }
+            }
+
             GameTemplate template;
             try
             {
-                template = GameTemplate.LoadGameTemplate("Templates/basic.gtp");
+                template = GameTemplate.LoadGameTemplate("Templates/" + templateFile + ".gtp");
             }
             catch (TemplateReadException ex)
             {
@@ -17,10 +37,11 @@ namespace GameIdeaGenerator
                 return;
             }
 
-            Random random = new Random();
-
-            string result = template.Generate(random);
-            Console.WriteLine(result);
+            for (int i = 0; i < count; i++)
+            {
+                string result = template.Generate(new Random());
+                Console.WriteLine(result);
+            }
         }
     }
 }
